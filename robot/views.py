bcedbeaf2ec2a162da47405dc3bdf9b99968fe19
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-09 20:53:40 +0100

    feat: create basic CRUD api views for Project, Robot, FK, IK models

--- robot/views.py
+++ robot/views.py
@@ -1,9 +1,108 @@
 from rest_framework import generics
 
 from .models import Project, Robot, ForwardKinematics, InverseKinematics
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, RobotSerializer, FkSerializer, IkSerializer
 
 
 class ProjectListAPIView(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class ProjectCreateAPIView(generics.CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectDetailAPIView(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+class ProjectDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        # instance
+        super().perform_destroy(instance)
+
+
+class RobotListAPIView(generics.ListAPIView):
+    queryset = Robot.objects.all()
+    serializer_class = RobotSerializer
+
+
+class RobotCreateAPIView(generics.CreateAPIView):
+    queryset = Robot.objects.all()
+    serializer_class = RobotSerializer
+
+
+class RobotDetailAPIView(generics.RetrieveAPIView):
+    queryset = Robot.objects.all()
+    serializer_class = RobotSerializer
+
+
+class RobotUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Robot.objects.all()
+    serializer_class = RobotSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+class RobotDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Robot.objects.all()
+    serializer_class = RobotSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        # instance
+        super().perform_destroy(instance)
+
+
+class FkCreateAPIView(generics.CreateAPIView):
+    queryset = ForwardKinematics.objects.all()
+    serializer_class = FkSerializer
+
+
+class FkDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = ForwardKinematics.objects.all()
+    serializer_class = FkSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+class IkCreateAPIView(generics.CreateAPIView):
+    queryset = InverseKinematics.objects.all()
+    serializer_class = IkSerializer
+
+
+class IkDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = InverseKinematics.objects.all()
+    serializer_class = IkSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
