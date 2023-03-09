c14c9b6a7caf4342e2c164f8fc79af248a9587f6
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-09 20:52:30 +0100

    feat: create basic serializers for Robot, FK, IK models

--- robot/serializers.py
+++ robot/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Robot, ForwardKinematics, InverseKinematics
 
 
 class ProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
+        fields = '__all__'
+
+
+class RobotSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Robot
+        fields = '__all__'
+
+
+class FkSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ForwardKinematics
+        fields = '__all__'
+
+
+class IkSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = InverseKinematics
         fields = '__all__'
