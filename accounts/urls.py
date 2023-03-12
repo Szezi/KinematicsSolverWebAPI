5c088411c4fdcb15a2607c8a285242ac04c7f876
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-12 17:51:21 +0100

    feat: add urls for register/detail/update user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import apiOverview, UserDetailAPIView, UserUpdateAPIView, RegisterUserAPIView
 
 urlpatterns = [
-
+    path('', apiOverview, name="api-overview"),
+    # path("login", .as_view(), name='login'),
+    # path("logout", .as_view(), name='logout'),
+    path('register', RegisterUserAPIView.as_view(), name='register'),
+    path("detail/<int:pk>", UserDetailAPIView.as_view(), name='detail'),
+    path("update/<int:pk>", UserUpdateAPIView.as_view(), name='update'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
