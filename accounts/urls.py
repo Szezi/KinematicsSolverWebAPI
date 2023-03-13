44f79acc84ed6d7bc9ada983366794c59a35f4b6
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-13 19:10:41 +0100

    feat: create API url for changing password

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
-from .views import apiOverview, UserDetailAPIView, UserUpdateAPIView, RegisterUserAPIView, LoginUserAPIView
+from .views import apiOverview, UserDetailAPIView, UserUpdateAPIView, RegisterUserAPIView, LoginUserAPIView, ChangePasswordAPIView
 
 urlpatterns = [
     path('', apiOverview, name="api-overview"),
     path("login", LoginUserAPIView.as_view(), name='login'),
     path("logout", LogoutView.as_view(next_page='login'), name='logout'),
     path('register', RegisterUserAPIView.as_view(), name='register'),
+    path('change-password', ChangePasswordAPIView.as_view(), name='change-password'),
     path("detail/<int:pk>", UserDetailAPIView.as_view(), name='detail'),
     path("update/<int:pk>", UserUpdateAPIView.as_view(), name='update'),
 ]
