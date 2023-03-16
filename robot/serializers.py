ab996564afd3a4a406a7eda1fa71c08d717a3d87
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-16 18:40:24 +0100

    feat: Create Project serializer with getting its robots feat: Create Robot serializer with getting its fk&ik calc.

--- robot/serializers.py
+++ robot/serializers.py
@@ -3,28 +3,41 @@ from .models import Project, Robot, ForwardKinematics, InverseKinematics
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class RobotSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Robot
         fields = '__all__'
 
 
 class FkSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = ForwardKinematics
         fields = '__all__'
 
 
 class IkSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = InverseKinematics
         fields = '__all__'
+
+
+class ProjectRobotsSerializer(serializers.ModelSerializer):
+    robots = RobotSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = ['name', 'admin', 'description', 'members', 'created', 'robots']
+
+
+class RobotsCalculationsSerializer(serializers.ModelSerializer):
+    fk_calc = FkSerializer()
+    ik_calc = IkSerializer()
+
+    class Meta:
+        model = Robot
+        fields = ['name', 'owner', 'description', 'notes', 'created', 'fk_calc', 'ik_calc']
