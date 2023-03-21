0e020703058fdbab98d3517baf6056961ee88053
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-21 20:30:21 +0100

    feat: create fk calculation func.

--- robot/utils.py
+++ robot/utils.py
@@ -7,3 +7,10 @@ def calculate_ik(links, x, y, z, alpha):
     config_1 = Robot_IK.ik_get_config1()
     config_2 = Robot_IK.ik_get_config2()
     return config_1, config_2
+
+
+def calculate_fk(links, theta1, theta2, theta3, theta4):
+    Robot_FK = RoboticArm(links)
+    result_xyz = Robot_FK.fk_solve_auto(theta1, theta2, theta3, theta4)
+    dh_table = Robot_FK.fk_dh(theta1, theta2, theta3, theta4)
+    return result_xyz, dh_table
