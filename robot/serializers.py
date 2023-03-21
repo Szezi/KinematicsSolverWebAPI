01ec0f395752d18128c09eb004caf204f957dd6c
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-03-21 19:11:38 +0100

    feat: create serializer for IK with robots data

--- robot/serializers.py
+++ robot/serializers.py
@@ -41,3 +41,25 @@ class RobotsCalculationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Robot
         fields = ['name', 'owner', 'description', 'notes', 'created', 'fk_calc', 'ik_calc']
+
+
+class InverseRobotsSerializer(serializers.ModelSerializer):
+    Robot = RobotSerializer(read_only=False)
+
+    class Meta:
+        model = InverseKinematics
+        fields = ['name', 'notes', 'created', 'modified', 'modified_by', 'x', 'y', 'z', 'alpha', 'theta1', 'theta11', 'theta2', 'theta22', 'theta3', 'theta33', 'theta4', 'theta44', 'Robot']
+
+    def update(self, instance, validated_data):
+        # CHANGE "userprofile" here to match your one-to-one field name
+        if 'Robot' in validated_data:
+            nested_serializer = self.fields['Robot']
+            nested_instance = instance.Robot
+            nested_data = validated_data.pop('Robot')
+
+            # Runs the update on whatever serializer the nested data belongs to
+            nested_serializer.update(nested_instance, nested_data)
+
+        # Runs the original parent update(), since the nested fields were
+        # "popped" out of the data
+        return super(InverseRobotsSerializer, self).update(instance, validated_data)
